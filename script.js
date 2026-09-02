f581ec1b709f2405e565dfd7f605218f840aa3de
Calvin-WD <calvinlamel@outlook.de>
2026-09-02 13:04:40 +0200

    feat: Add dialog functionality to the board
    - add dialog basic styling to components.css
    - add open and close functions for  the dialog in script.js
    - add the templates and get template functions for task cards and task overlay in templates.js and script.js
    - switch board add-button from mobile to desktop version by desktop breakpoint
    - rename the add-button classes in components.css
    - add some css utility classes in global_styles.css

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function hideContactDetails() {
     document.documentElement.classList.remove("contact-details-open");
     document.body.classList.remove("contact-details-open");
 }
+
+function openDialogTask() {
+    const dialogRef = document.getElementById("dialog-task");
+
+    dialogRef.innerHTML = getTaskOverlayTemplate();
+    document.body.classList.add("overflow-hidden")
+    
+    dialogRef.showModal();
+}
+
+function closeDialogTask() {
+    const dialogRef = document.getElementById("dialog-task");
+
+    document.body.classList.remove("overflow-hidden")
+    
+    dialogRef.close();
+}
---
+++ scripts/templates.js
@@ -0,0 +1,92 @@
+function getTaskCardTemplate() {
+    return `<button type="button" class="task-card">
+                <span class="badge-type badge-type-user">User Story</span>
+                <div class="task-card-body">
+                    <h3 class="task-card-title">HTML Base Template Creation</h3>
+                    <div class="task-card-description">
+                        Lorem ipsum dolor sit amet, consectetur.
+                    </div>
+                </div>
+                <div class="task-card-subtasks">
+                    <div class="task-card-progress">
+                        <div class="task-card-progress-bar"></div>
+                    </div>
+                    <span class="task-card-summary">1/4 Subtasks</span>
+                </div>
+                <div class="task-card-assignees">
+                    <ul class="task-card-users">
+                        <li class="badge-user badge-user-orange">
+                            <span>CL</span>
+                        </li>
+                        <li class="badge-user badge-user-blue-light">
+                            <span>SS</span>
+                        </li>
+                        <li class="badge-user badge-user-mint">
+                            <span>ND</span>
+                        </li>
+                    </ul>
+                    <span><img src="../assets/icons/prio_low.svg" alt="Priority low" /></span>
+                </div>
+            </button>`;
+}
+
+function getTaskOverlayTemplate() {
+    return `<div class="task-overlay">
+                <header class="task-overlay-header">
+                    <div class="task-overlay-type-wrapper">
+                        <span class="badge-type badge-type-user">User Story</span>
+                        <button class="button button-close" onclick="closeDialogTask()">
+                            <img src="../assets/icons/cancel.svg" alt="Close" />
+                        </button>
+                    </div>
+                    <h2>HTML Base Template Creation</h2>
+                </header>
+                <div class="task-overlay-body">
+                    <p class="task-overlay-description">Build start page with recipe recommendation.</p>
+                    <dl class="task-overlay-details">
+                        <dt>Due Date:</dt>
+                        <dd>10/05/2023</dd>
+                        <dt>Priority:</dt>
+                        <dd>
+                            <div class="d-flex-align-center gap-xs">
+                                <span>Medium</span>
+                                <img src="../assets/icons/prio_medium.svg">
+                            </div>
+                        </dd>
+                        <dt class="task-overlay-assignees">Assigned To:</dt>
+                        <dd>
+                            <ul>
+                                <li>
+                                    <span class="badge-user badge-user-orange">CL</span
+                                    ><span>Calvin Lamel</span>
+                                </li>
+                                <li>
+                                    <span class="badge-user badge-user-blue-light">SS</span
+                                    ><span>Sebastian Stock</span>
+                                </li>
+                                <li>
+                                    <span class="badge-user badge-user-mint">ND</span><span>Norman Domann</span>
+                                </li>
+                            </ul>
+                        </dd>
+                        <dt class="task-overlay-subtasks">Subtasks</dt>
+                        <dd>
+                            <ul>
+                                <li><input type="checkbox" />Implement Recipe Recommendation</li>
+                                <li><input type="checkbox" />Start Page Layout</li>
+                            </ul>
+                        </dd>
+                    </dl>
+                </div>
+                <footer class="task-overlay-footer">
+                    <button class="button button-task-overlay">
+                        <img src="../assets/icons/delete.svg" alt="Delete" />
+                        <span>Delete</span>
+                    </button>
+                    <button class="button button-task-overlay">
+                        <img src="../assets/icons/edit.svg" alt="Edit" />
+                        <span>Edit</span>
+                    </button>
+                </footer>
+            </div>`;
+}
