ef331e062b109424d3177de73a5fc88cd1fa5f9f
Calvin-WD <calvinlamel@outlook.de>
2026-09-11 16:01:37 +0200

    feat: Improve navbar design and dialog  handling
    - add responsive header and profile navigation
    - add mobile contact edit dialog
    - generalize dialog close handling
    - align help and legal pages with app layout

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ function openTaskDialog() {
     openDialog("dialog-task");
 }
 
-function requestCloseTaskDialog() {
-    const dialogRef = document.getElementById("dialog-task");
+function requestCloseDialog(dialogId) {
+    const dialogRef = document.getElementById(dialogId);
 
     dialogRef.requestClose();
 }
@@ -58,23 +58,23 @@ function closeTaskDialog(event) {
 
     const dialogRef = document.getElementById("dialog-task");
 
-    dialogRef.classList.remove("dialog-task-closing");
+    dialogRef.classList.remove("dialog-closing-to-right");
     document.body.classList.remove("overflow-hidden");
 
     closeDialog("dialog-task");
 }
 
-function closeProfileDialog(event) {
+function closeProfileDialog(event, dialogId) {
     if (event.animationName !== "slide-out-to-right") {
         return;
     }
 
-    const dialogRef = document.getElementById("dialog-profile");
+    const dialogRef = document.getElementById(dialogId);
 
     dialogRef.classList.remove("dialog-profile-closing");
     document.body.classList.remove("overflow-hidden");
 
-    closeDialog("dialog-profile");
+    closeDialog(dialogId);
 }
 
 // function dialogTaskSlideOut(event) {
--- scripts/templates.js
+++ scripts/templates.js
@@ -35,7 +35,7 @@ function getTaskOverlayTemplate() {
                 <header class="task-overlay-header">
                     <div class="task-overlay-type-wrapper">
                         <span class="badge-type badge-type-user">User Story</span>
-                        <button class="button button-close" onclick="requestCloseTaskDialog()" aria-label="Close Task">
+                        <button class="button button-close" onclick="requestCloseDialog('dialog-task')" aria-label="Close Task">
                             <img src="../assets/icons/cancel.svg" alt="Close" />
                         </button>
                     </div>
